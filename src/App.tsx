a681c3cfb68c6e6b96bf79cfb242479ba60853e8
MaxenceGitDev <maxence.pruvot@icloud.com>
2024-12-12 16:21:47 +0100

    rendering-list.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import PokemonCard from "./components/PokemonCard";
 import { useState } from "react";
 
 export default function App() {
-  const [pokemonIndex, setPokemonIndex] = useState(0);
-
   const pokemonList = [
     {
+      id: 1,
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      id: 2,
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
+      id: 3,
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
     },
     {
+      id: 4,
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
@@ -31,39 +33,23 @@ export default function App() {
     },
   ];
 
-  // const handleClickS = () => {
-  //   setPokemonIndex((index) =>
-  //     index < pokemonList.length - 1 ? index + 1 : index
-  //   )};
-
-  // const handleClickP = () => {
-  //   setPokemonIndex((index) => (index > 0 ? index - 1 : index));
-  // };
-
-  const handleClickS = () => {
-    setPokemonIndex((index) => index + 1);
-  };
-
-  const handleClickP = () => {
-    setPokemonIndex((index) => index - 1);
-  };
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
   return (
     <div>
-      <PokemonCard
-        name={pokemonList[pokemonIndex].name}
-        imgSrc={pokemonList[pokemonIndex].imgSrc}
-      />
-      {pokemonIndex > 0 && (
-        <button type="button" onClick={handleClickP}>
-          Précédent
-        </button>
-      )}
-      {pokemonIndex < pokemonList.length - 1 && (
-        <button type="button" onClick={handleClickS}>
-          Suivant
-        </button>
-      )}
+      <nav>
+        {pokemonList.map((pokemon, index) => (
+          <button
+            type="button"
+            key={pokemon.id}
+            onClick={() => setPokemonIndex(index)}
+          >
+            {pokemon.name}
+          </button>
+        ))}
+        ;
+      </nav>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,14 @@
 interface PokemonCardProps {
-  name: string;
-  imgSrc?: string;
+  pokemon: {
+    name: string;
+    imgSrc?: string;
+  };
 }
-export default function PokemonCard({ name, imgSrc }: PokemonCardProps) {
+export default function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
     <figure>
-      {imgSrc ? <img src={imgSrc} alt={name} /> : <p>???</p>}
-      <figcaption>{name}</figcaption>
+      {pokemon ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
