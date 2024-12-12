e51f14e1a291e9a741653c79443025a8b2e0be9b
MaxenceGitDev <maxence.pruvot@icloud.com>
2024-12-12 12:27:18 +0100

    state.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,69 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 export default function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  // const handleClickS = () => {
+  //   setPokemonIndex((index) =>
+  //     index < pokemonList.length - 1 ? index + 1 : index
+  //   )};
+
+  // const handleClickP = () => {
+  //   setPokemonIndex((index) => (index > 0 ? index - 1 : index));
+  // };
+
+  const handleClickS = () => {
+    setPokemonIndex((index) => index + 1);
+  };
+
+  const handleClickP = () => {
+    setPokemonIndex((index) => index - 1);
+  };
+
   return (
     <div>
-      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+      <PokemonCard
+        name={pokemonList[pokemonIndex].name}
+        imgSrc={pokemonList[pokemonIndex].imgSrc}
+      />
+      {pokemonIndex > 0 && (
+        <button type="button" onClick={handleClickP}>
+          Précédent
+        </button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button type="button" onClick={handleClickS}>
+          Suivant
+        </button>
+      )}
     </div>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbizarre",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
